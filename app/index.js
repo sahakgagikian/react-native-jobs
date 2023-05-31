f1da8ac8b744de786874f433505fd17209667a1d
sahakgagikian <sgagikyan@gmail.com>
2023-05-31 19:13:54 +0400

    initial commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import { Nearbyjobs, ScreenHeaderBtn, Welcome } from "../components";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
